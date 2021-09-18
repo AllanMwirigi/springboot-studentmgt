7966661798537b9d1733ac98084ec58e87753cac
AllanMwirigi <allo.mwirigi@gmail.com>
2021-09-18 19:07:44 +0300

    repository data access layer

--- src/main/java/com/spring/studentmgt/student/StudentService.java
+++ src/main/java/com/spring/studentmgt/student/StudentService.java
@@ -4,15 +4,24 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service // allows for dependecy injection where this class is used
 public class StudentService { // Service layer
 
-  public List<Student> getStudents() {
-		return List.of(
-            new Student(1L, "John", "john@mail.com", LocalDate.of(2000, Month.JANUARY, 13), 29)
-        );
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+		// return List.of(
+        //     new Student(1L, "John", "john@mail.com", LocalDate.of(2000, Month.JANUARY, 13), 29)
+        // );
 	}
   
 }
